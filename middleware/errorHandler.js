f54a72fdb35ce8e8d586790322a020392a7afcdb
forsaken <112668441+defy-irys@users.noreply.github.com>
2026-04-08 11:17:18 +0800

    Add files via upload

---
+++ middleware/errorHandler.js
@@ -0,0 +1,20 @@
+﻿const { sendResponse } = require('../utils/response');
+
+class AppError extends Error {
+  constructor(message, status = 500) {
+    super(message);
+    this.status = status;
+  }
+}
+
+function notFound(req, res, next) {
+  next(new AppError('Route not found.', 404));
+}
+
+function errorHandler(err, req, res, next) {
+  const status = err.status || 500;
+  const message = err.message || 'Server error.';
+  return sendResponse(res, { success: false, data: null, message }, status);
+}
+
+module.exports = { AppError, notFound, errorHandler };
---
+++ utils/response.js
@@ -0,0 +1,5 @@
+﻿function sendResponse(res, { success, data = null, message = '' }, status = 200) {
+  return res.status(status).json({ success, data, message });
+}
+
+module.exports = { sendResponse };
