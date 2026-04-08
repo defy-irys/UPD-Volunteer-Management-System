ad328561f148f1ad38a930f5bb4e47f8417de753
forsaken <112668441+defy-irys@users.noreply.github.com>
2026-04-08 11:28:35 +0800

    Delete utils directory

--- utils/response.js
+++
@@ -1,5 +0,0 @@
-﻿function sendResponse(res, { success, data = null, message = '' }, status = 200) {
-  return res.status(status).json({ success, data, message });
-}
-
-module.exports = { sendResponse };
